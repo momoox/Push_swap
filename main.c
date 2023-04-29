2f6883773bf3ba52e972a7cdf226aec640545a97
Momox <119704058+momoox@users.noreply.github.com>
2023-04-29 12:34:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,18 +7,18 @@ int main(int argc, char **argv)
   t_stacks *s;
 	int 	i;
 
-    list = ft_calloc(sizeof(int), (argc - 1));
-    if (valid_check(argv, argc, list) == NULL)
+    s = ft_calloc(sizeof(int), (argc - 1));
+    if (valid_check(argv, argc, s) == NULL)
 		return(0);
-	if (list == NULL)
+	if (s == NULL)
 	{
-		errorexit(list);
+		errorexit(s);
 		return (0);
 	}
 	i = 0;
     while(i < argc - 1)
 	{
-		printf("%d ", list[i]);
+		printf("%d ", s[i]);
 	 	i++;
 	}
 	printf("\n");
